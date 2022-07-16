1e4054e77fc0d137d0e45ce826d0123ec8a74842
adamsocki <socki.adam@gmail.com>
2022-07-16 18:57:57 +0200

    Passing data to a child.

--- Landmarks/Views/ContentView.swift
+++ Landmarks/Views/ContentView.swift
@@ -9,19 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack (alignment: .leading) {
-            Text("Turtle Rock")
-                .font(.title)
-            HStack {
-                Text("Joshua Tree National Park")
-                    .font(.subheadline)
-                Spacer()
-                Text("California")
-                    .font(.subheadline)
-            }
-            
-        }
-        .padding()
+        LandmarkList()
     }
 }
 
